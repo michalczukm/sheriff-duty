4c0da8595f5664032f7de8f3fde50f876747d1e4
Michal Michalczuk <michalczukm@gmail.com>
2024-02-09 02:22:43 +0100

    first simple implementation of sheriff bot with cloudflare KV as storage

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,79 @@
 import { Router } from 'itty-router';
 
-// now let's create a router (note the lack of "new")
 const router = Router();
 
-// GET collection index
-router.get('/api/todos', () => new Response('Todos Index!'));
-
-// GET item
-router.get('/api/todos/:id', ({ params }) => new Response(`Todo #${params.id}`));
-
-// POST to the collection (we'll use async here)
-router.post('/api/todos', async (request) => {
+// TODO: add slack requests verification using https://api.slack.com/authentication/verifying-requests-from-slack#making
+router.post('/api/events', async (request, env: Env) => {
+	console.log('here!', new Date().toISOString());
 	const content = await request.json();
 
-	return new Response('Creating Todo: ' + JSON.stringify(content));
+	if (content.type == 'url_verification' && content?.challenge) {
+		return new Response(content.challenge);
+	}
+	switch (content.event.type) {
+		case 'app_mention':
+			const sheriffUserId = await env.SHERIFF_DUTY.get(`${content['team_id']}_sheriff`);
+
+			await fetch('https://slack.com/api/chat.postMessage', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json; charset=utf-8',
+					Authorization: `Bearer ${env.SLACK_BOT_OAUTH_TOKEN}`,
+				},
+				body: JSON.stringify({
+					channel: content.event.channel,
+					icon_emoji: ':robot_face:',
+					text: `Hey <@${sheriffUserId}>, looks like you are a sheriff 🪪 around here!`,
+					username: 'sheriff-bot',
+				}),
+			});
+			break;
+		default:
+			break;
+	}
+
+	return new Response('', { status: 200 });
+});
+
+const USER_REGEX = /\<@(?<user_id>\w*)\|(?<username>\w*)>$/;
+const make400Response = () =>
+	new Response(
+		JSON.stringify({
+			response_type: 'in_channel',
+			text: `Please provide a user to search for.`,
+		}),
+		{ headers: { 'Content-type': 'application/json' }, status: 400 }
+	);
+
+router.post('/api/interactions', async (request, env: Env) => {
+	const content = await request.formData();
+	const textParam = content.get('text');
+	const teamId = content.get('team_id');
+
+	if (!textParam || typeof textParam !== 'string' || !teamId || typeof teamId !== 'string') {
+		return make400Response();
+	}
+
+	const match = textParam.match(USER_REGEX);
+	if (!match) {
+		return make400Response();
+	}
+
+	const userId = match.groups?.['user_id'];
+
+	if (!userId) {
+		return make400Response();
+	}
+
+	await env.SHERIFF_DUTY.put(`${teamId}_sheriff`, userId);
+
+	return new Response(
+		JSON.stringify({
+			response_type: 'in_channel',
+			text: `Ok! <@${userId}> is now a sheriff 🪪 around here!`,
+		}),
+		{ headers: { 'Content-type': 'application/json' } }
+	);
 });
 
 // 404 for everything else
--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,6 +1,6 @@
 interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	SLACK_BOT_OAUTH_TOKEN: string;
+	SHERIFF_DUTY: KVNamespace;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
